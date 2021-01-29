ab31f8c7b1c22a9669a1f316ba6c1532ea301f22
tonezone108 <tonezone108@gmail.com>
2021-01-28 19:29:39 -0600

    backend is almost done

--- Server/controllers/moods.js
+++ Server/controllers/moods.js
@@ -53,6 +53,40 @@ const getAllMoods = (req, res) => {
    })
  }
 
+ const updateMoodByDateAndUsername = (req, res) => {
+  const {  username, mood, date, description } = req.body
+  let sql = "UPDATE moods SET mood = ?, description =? WHERE date = ? AND username = ?"
+  sql = mysql.format(sql, [mood, description, date, username ])
+  //console.log(sql);
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json({ newId: results.insertId });
+  })
+}
+
+
+const deleteMoodByDateAndUsername = (req, res) => {
+  const {  username, date } = req.body
+  let sql = "DELETE FROM moods WHERE date = ? AND username = ?"
+  sql = mysql.format(sql, [date, username ])
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
+  })
+}
+
+
+// const updateUserById = (req, res) => {
+//   const { firstName, lastName } = req.body
+//   let sql = "UPDATE login_app_users SET first_name = ?, last_name = ? WHERE id = ?"
+//   sql = mysql.format(sql, [ firstName, lastName, req.params.id ])
+
+//   pool.query(sql, (err, results) => {
+//     if (err) return handleSQLError(res, err)
+//     return res.status(204).json();
+//   })
+// }
 
 
 
@@ -68,5 +102,7 @@ const getAllMoods = (req, res) => {
     getMoodById,
     getAllMoodsByUsername,
     getAllMoodsByDate,
+    updateMoodByDateAndUsername,
+    deleteMoodByDateAndUsername,
     createMood
   }
--- Server/routers/moods.js
+++ Server/routers/moods.js
@@ -14,11 +14,11 @@ router.get("/date/:date", authenticate, moodsController.getAllMoodsByDate);
 
 router.post("/create", authenticate, moodsController.createMood);
 
-// router.put("/date", authenticate, usersController.updateMoodByDate);
+router.put("/update", authenticate, moodsController.updateMoodByDateAndUsername);
 // router.put("/user", authenticate, usersController.updateMoodByUser);
 
-// router.delete(
-//   "/:date", authenticate, usersController.deleteMoodByDate
-// );
+router.delete(
+   "/delete", authenticate, moodsController.deleteMoodByDateAndUsername
+ );
 
 module.exports = router;
